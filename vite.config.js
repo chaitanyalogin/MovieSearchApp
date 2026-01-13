e35a92ff0a0b8999f121d46c04c0c71454efb1d9
chayy <chaitanyarocks.kulkarni@gmail.com>
2026-01-13 16:08:40 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/movieapp/',   // 🔴 REQUIRED
+  base: '/MovieSearchApp/', // ✅ MUST match repo name exactly
   plugins: [
     react(),
     tailwindcss(),
